babccdba059ffe876778d87279882cec842626be
Jia_en <73224428+nyx-ulrix@users.noreply.github.com>
2025-02-27 16:50:17 +0800

    fixed error

--- malcolm-portfolio/src/main.tsx
+++ malcolm-portfolio/src/main.tsx
@@ -1,8 +1,6 @@
-// src/main.tsx
 import React, { useEffect } from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-
 import './styles/global.css';
 import Lenis from '@studio-freight/lenis';
 import { Provider } from 'react-redux';
@@ -10,33 +8,34 @@ import store from './store';
 
 const container = document.getElementById('root');
 
+if (container) {
+  const root = ReactDOM.createRoot(container);
 
-const Main: React.FC = () => {
-  useEffect(() => {
-    const lenis = new Lenis({
-      duration: 1.2,
-      easing: (t: number) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
-      // smooth: true,
-      // direction: 'vertical',
-    });
+  const Main: React.FC = () => {
+    useEffect(() => {
+      const lenis = new Lenis({
+        duration: 1.2,
+        easing: (t: number) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
+      });
 
-    const raf = (time: number) => {
-      lenis.raf(time);
+      const raf = (time: number) => {
+        lenis.raf(time);
+        requestAnimationFrame(raf);
+      };
+
       requestAnimationFrame(raf);
-    };
 
-    requestAnimationFrame(raf);
+      return () => {
+        lenis.destroy();
+      };
+    }, []);
 
-    return () => {
-      lenis.destroy();
-    };
-  }, []);
+    return (
+      <Provider store={store}>
+        <App />
+      </Provider>
+    );
+  };
 
-  return (
-    <Provider store={store}>
-      <App />
-    </Provider>
-  );
-};
-
-ReactDOM.render(<Main />, document.getElementById('root'));
+  root.render(<Main />);
+}
